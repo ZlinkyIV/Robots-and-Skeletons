074118bbf654088e87ee02922c323b1a83185e87
Kaydon <kaydon@Kaydons-MacBook-Air.local>
2023-04-28 10:21:31 -0600

    Installed and started with Unity Test Framework.

---
+++ Assets/Simulation/Tests/FacadeTest.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class FacadeTest
+{
+    [Test]
+    public void FacadeTestSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+}
